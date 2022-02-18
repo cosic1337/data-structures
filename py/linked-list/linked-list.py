b3faafd51edd23ae0a8c4f7e953db87f00a34e77
cosic1337 <cosic.uros1337@gmail.com>
2022-02-18 18:10:34 +0100

    improved code

--- py/linked-list/linked-list.py
+++ py/linked-list/linked-list.py
@@ -2,29 +2,20 @@ from singly import SinglyLinkedList as singly
 from doubly import DoublyLinkedList as doubly
 from helpers import print_nodes, generate_nodes, del_nodes
 
-def singly_ll():
-    sl = singly()
-    generate_nodes(30, sl)
-    assert sl.size() == 30
-    head, tail = sl.front(), sl.back()
-    sl.reverse()
-    assert head == sl.back() and tail == sl.front()
-    del_nodes(10, sl)
-    assert sl.size() == 20
-
-def doubly_ll():
-    dl = doubly()
-    generate_nodes(30, dl)
-    assert dl.size() == 30
-    head, tail = dl.front(), dl.back()
-    dl.reverse()
-    assert head == dl.back() and tail == dl.front()
-    del_nodes(10, dl)
-    assert dl.size() == 20
+def test_list(list=None, size=0, del_size=0):
+    generate_nodes(size, list)
+    assert list.size() == size
+    head, tail = list.front(), list.back()
+    list.reverse()
+    assert head == list.back() and tail == list.front()
+    del_nodes(del_size, list)
+    assert list.size() == size - del_size
 
 def main():
-    singly_ll()
-    doubly_ll()
+    sl = singly()
+    dl = doubly()
+    test_list(sl, 30, 10)
+    test_list(dl, 30, 10)
 
 if __name__ == '__main__':
     main()
