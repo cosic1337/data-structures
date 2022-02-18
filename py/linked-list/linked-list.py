ed0e35f8fbd3607512dc81f0f444a1f232e1fda5
cosic1337 <cosic.uros1337@gmail.com>
2022-02-18 17:37:02 +0100

    Added Doubly Linked List and improved code

--- py/linked-list/linked-list.py
+++ py/linked-list/linked-list.py
@@ -2,6 +2,7 @@ class ListNode:
     def __init__(self, val):
         self.val = val
         self.next = None
+        self.prev = None
 
 class SinglyLinkedList:
     def __init__(self):
@@ -35,7 +36,7 @@ class SinglyLinkedList:
         self._size += 1
 
     def pop_front(self):
-        if not self.head:
+        if self.empty():
             return None
         tmp = self.head
         self.head = self.head.next
@@ -72,7 +73,7 @@ class SinglyLinkedList:
         return tmp
 
     def front(self):
-        if not self.head:
+        if not self.empty():
             return None
         return self.head.val
 
@@ -144,36 +145,208 @@ class SinglyLinkedList:
             curr = next
         self.head = prev
 
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self._size = 0
+
+    def size(self):
+        return self._size
+
+    def empty(self):
+        return self.head == None
+
+    def value_at(self, idx):
+        if self.empty():
+            return None
+        if idx >= self.size():
+            raise IndexError('Index out of range')
+        if idx > self.size() // 2:
+            curr = self.tail
+            i = self.size() - 1
+            while curr:
+                if i == idx:
+                    return curr.val
+                i -= 1
+                curr = curr.prev
+        else:
+            curr = self.head
+            i = 0
+            while curr:
+                if i == idx:
+                    return curr.val
+                i += 1
+                curr = curr.next
+
+    def push_front(self, val):
+        n = ListNode(val)
+        if self.empty():
+            self.head = self.tail = n
+            self._size += 1
+        else:
+            tmp = self.head
+            self.head = n
+            n.next = tmp
+            tmp.prev = n
+            self_size += 1
+
+    def pop_front(self):
+        if self.empty():
+            return None
+        tmp, tmp_val = self.head, self.head.val
+        self.head = self.head.next
+        self.head.prev = tmp = None
+        self._size -= 1
+        return tmp_val
+
+    def push_back(self, val):
+        if self.empty():
+            self.push_front(val)
+        else:
+            n = ListNode(val)
+            tmp = self.tail
+            self.tail = n
+            tmp.next = n
+            n.prev = tmp
+            self._size += 1
+
+    def pop_back(self):
+        if self.empty():
+            return None
+        if self.size() == 1:
+            tmp = self.head.val
+            self.head = self.tail = None
+            self._size -= 1
+            return tmp
+        tmp, tmp_val = self.tail, self.tail.val
+        self.tail = self.tail.prev
+        self.tail.next = tmp = None
+        self._size -= 1
+        return tmp_val
+    
+    def front(self):
+        if self.empty():
+            return None
+        return self.head.val
+
+    def back(self):
+        if self.empty():
+            return None
+        return self.tail.val
+    
+    def insert(self, idx, val):
+        if idx > self.size():
+            raise IndexError('Index out of range')
+        if idx == 0:
+            self.push_front(val)
+        elif idx == self.size():
+            self.push_back(val)
+        else:
+            if(idx > self.size() // 2):
+                i = self.size() - 2
+                curr = self.tail.prev
+                while curr:
+                    if idx == i:
+                        n = ListNode(val)
+                        curr.next.prev = n
+                        n.next = curr.next.prev
+                        curr.next = n
+                        n.prev = curr
+                        self._size += 1
+                        return n.val
+                    i -= 1
+                    curr = curr.prev
+            else:
+                curr = self.head.next
+                i = 1
+                while curr:
+                    if i == idx:
+                        n = ListNode(val)
+                        curr.next.prev = n
+                        n.next = curr.next.prev
+                        curr.next = n
+                        n.prev = curr
+                        self._size += 1
+                        return n.val
+                    i += 1
+                    curr = curr.next
+
+    def erase(self, idx):
+        if self.empty():
+            return None
+        if idx >= self.size():
+            raise IndexError('Index out of range')
+        if idx == 0:
+            return self.pop_front()
+        if idx == self.size() - 1:
+            return self.pop_back()
+        if idx > self.size() // 2:
+            curr = self.tail.prev
+            i = self.size() - 2
+            while curr:
+                if i == idx:
+                    tmp_val = curr.val
+                    curr.prev.next = curr.next
+                    curr.next.prev = curr.prev
+                    curr = curr.val = None
+                    self._size -= 1
+                    return tmp_val
+                i -= 1
+                curr = curr.prev
+        else:
+            curr = self.head.next
+            i = 1
+            while curr:
+                if i == idx:
+                    tmp_val = curr.val
+                    curr.prev.next = curr.next
+                    curr.next.prev = curr.prev
+                    curr = curr.val = Nonje
+                    self._size -= 1
+                    return tmp_val
+                i += 1
+                curr = curr.next
+
+    def reverse(self):
+        if self.empty():
+            raise Exception('Can\'t reverse empty list')
+        if self.size() == 1:
+            return
+        curr = self.head
+        prev = self.head.prev
+        while curr:
+            next = curr.next
+            curr.next = prev
+            curr.prev = next
+            prev = curr
+            curr = next
+        tmp = self.head
+        self.head = self.tail
+        self.tail = tmp
+
+def print_nodes(head):
+    if not head:
+        return
+    print(f'{head.val}=>', end='')
+    print_nodes(head.next)
+        
+
+def singly():
+    pass
+
+def doubly():
+    dl = DoublyLinkedList()
+    for i in range(10):
+        dl.push_back(i)
+    print_nodes(dl.head)
+    dl.reverse()
+    print()
+    print_nodes(dl.head)
 
 def main():
-    linked_list = SinglyLinkedList()
-    for i in range(50):
-        linked_list.push_back((i**2) % 16)
-    
-    assert linked_list.size() == 50
-
-    for _ in range(5):
-        linked_list.pop_back()
-        linked_list.pop_front()
-    
-    assert linked_list.size() == 40
-    
-    print(f'Head: {linked_list.front()}')
-    print(f'Tail: {linked_list.back()}')
-
-    linked_list.insert(linked_list.size() // 2, 'middle')
-
-    assert linked_list.size() == 41
-
-    linked_list.erase(linked_list.size() // 2 - 1)
-
-    assert linked_list.size() == 40
-
-    head, tail = linked_list.front(), linked_list.back()
-    linked_list.reverse()
-    print(f'After reverse():\nHead: {tail}\nTail: {head}')
-
-    assert linked_list.front() == tail and linked_list.back() == head
+    singly()
+    doubly()
 
 if __name__ == '__main__':
     main()
