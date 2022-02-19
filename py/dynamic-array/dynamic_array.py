a9c321adcbde041bd789ec375322021377818766
cosic1337 <cosic.uros1337@gmail.com>
2022-02-19 15:19:57 +0100

    tidying up

--- py/dynamic-array/dynamic_array.py
+++ py/dynamic-array/dynamic_array.py
@@ -11,7 +11,7 @@ class DynamicArray:
 
     def push(self, val):
         if self._size == self.capacity:
-            self.allocate_new_arr()
+            self.resize()
         self.arr[self._size] = val
         self._size += 1
     
@@ -23,14 +23,25 @@ class DynamicArray:
         self._size -= 1
         return tmp
 
-    def remove(self, idx):
+    def delete(self, idx):
         if idx < 0 or idx >= self._size:
             raise IndexError('Index out of range')
         for i in range(idx, self._size - 1):
             self.arr[j] = self.arr[j+1]
         self._size -= 1
 
-    def allocate_new_arr(self):
+    def remove(self, item):
+        for i in range(self.size()):
+            if self.arr[i] == item:
+                self.delete(i)
+
+    def find(self, item):
+        for i in range(self.size()):
+            if self.arr[i] == item:
+                return self.arr[i]
+        return -1
+
+    def resize(self):
         self.capacity = self.capacity * 2
         new_arr = [None] * self.capacity
         for i in range(self._size):
@@ -39,6 +50,12 @@ class DynamicArray:
 
     def size(self):
         return self._size
+
+    def capacity(self):
+        return self.capacity
+
+    def isempty(self):
+        return self._size == 0
 
     def __len__(self):
         return self._size
--- py/dynamic-array/readme.md
+++ py/dynamic-array/readme.md
@@ -1,7 +1,3 @@
 # Simple dynamic_array implementation
 
-without handling resize if too much elements have been removed
-
-non iterable with basic for loop (not handling None items)
-
-will possibly add in future
+Might make it iterable in future and lower the size if too many elements were removed
